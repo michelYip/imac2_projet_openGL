a4ec9f8237d9f5fc02626791119f2fb1f6046281
Léa Harabagiu <elessach@gmail.com>
2019-01-06 00:43:30 +0100

    adapted character

--- GLImac-Template/projet/include/TrackballCamera.hpp
+++ GLImac-Template/projet/include/TrackballCamera.hpp
@@ -15,7 +15,7 @@ class TrackballCamera : public Camera{
 
 	public:
 		// constructeur(s)
-		TrackballCamera():_fDistance(-10.f), _fAngleY(180.f),_fAngleX(20.f){}; 
+		TrackballCamera():_fDistance(-5.f), _fAngleY(180.f),_fAngleX(15.f){}; 
 
 		// getters
 		inline const float getDistance() const{
